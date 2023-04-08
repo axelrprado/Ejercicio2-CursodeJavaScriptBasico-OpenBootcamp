8e27fe0fd3185949e47de13bcb8e6faf7bc8e936
Axel Rodriguez <axelrprado@gmail.com>
2023-04-07 20:07:21 -0600

    Ejercicio2

---
+++ Ejercicio2.js
@@ -0,0 +1,16 @@
+const fecNac = new Date(1997,7,20)
+const libro = {
+        titulo: "Atomic Habits",
+        autor: "James Clear",
+        fecha: "20-01-01",
+        url: "http://google.com"
+    }
+const lista = [
+    "Axel David",
+    25,
+    false,
+    fecNac,
+    libro
+];
+
+console.log(lista);
